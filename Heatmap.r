3097b83871bb4d96b10ed960efc817c9da8d910f
Namuun Battur <61587650+NmnBttr@users.noreply.github.com>
2024-03-06 01:05:00 +0100

    Update Heatmap.r

--- Heatmap.r
+++ Heatmap.r
@@ -2,7 +2,7 @@
 rm(list = ls())
 
 # set working directory
-setwd("/home/namuun/Documents/Polio_Nanopore_Analysis/snpeff_tabular")
+setwd("path/to/heatmap_tabular")
 
 # library install and import
 
@@ -31,7 +31,7 @@ number_of_clusters <- 3             ## do you assume a particular amount of clus
 
 # data preparation
 
-## import data
+## import data --- pattern = "SiB159_*""*.tabular"
 files<-list.files(path = ".",pattern = "*.tabular", recursive = F ,full.names = TRUE)
 
 ## only in case it contains a date:
@@ -41,8 +41,8 @@ if(date) {
 
 ## create data.frame for first dataset
 variants<-read.table(files[1], header = T, sep = "\t",stringsAsFactors=FALSE, 
-                     colClasses = c("character"))
-final<- paste0(variants$POS, variants$ALT, if(display_as){paste0(" ", "(", variants$EFF....AA, ")")})
+                     colClasses = c("character"), fill = T)
+final<- paste0(variants$POS, variants$ALT)
 final<- data.frame(cbind(final, variants$AF))
 Sample_number<-path_file(files[1])
 Sample_number = file_path_sans_ext(Sample_number)
@@ -52,8 +52,8 @@ colnames(final) <- c("Mutation", Sample_number)
 ## loop over all other datasets and join by Mutation
 for(i in 2:length(files)) {
   variants<-read.table(files[i], header = T, sep = "\t",stringsAsFactors=FALSE, 
-                       colClasses = c("character"))
-  varpos<- paste0(variants$POS, variants$ALT, if(display_as){paste0(" ", "(", variants$EFF....AA, ")")})
+                       colClasses =c("character"), fill = T, skipNul = TRUE)
+  varpos<- paste0(variants$POS, variants$ALT)
   varpos<- cbind(varpos, variants$AF)
   Sample_number<-path_file(files[i])
   Sample_number = file_path_sans_ext(Sample_number)
@@ -65,6 +65,7 @@ for(i in 2:length(files)) {
 final<-final[str_order(final$Mutation, numeric = T),]
 
 ## set row names
+# rownames(diasyhoras2) <- make.names(diasyhoras[,1], unique = TRUE)
 row.names(final)<-final$Mutation
 final<-final[,-1]
 
@@ -73,7 +74,7 @@ final<-final[,-1]
 if(number){final<-final[,str_order(colnames(final), numeric = T)]}
 
 ## adjust the variant frequency:
-final[final < frequency] <- NA
+#final[final < frequency] <- NA
 
 final <- final[rowSums(is.na(final)) !=ncol(final), ]
 final <- t(final)
@@ -86,17 +87,17 @@ if(percent){final<-final*100}
 # show rounded values in pheatmap
 if(rounded){final_rounded <- round(final, 0)} else {final_rounded <- F}
 
-# add annotations
+# add annotations for Polio 1. For Polio 2 and 3 adjust protein positions
 
 ## readout annotations
-ann_final <- read.table(files[1], header = T, sep = "\t") 
+ann_final <- read.table(files[1], header = T, sep = "\t", fill = T) 
 # ann_final <- data.frame(paste0(ann_final$POS, ann_final$ALT, if(display_as){paste0(" ", "(", ann_final$EFF....AA, ")")}), ann_final$EFF....EFFECT, ann_final$EFF....GENE)
 # colnames(ann_final) <- c("position","effect", "gene")
 ann_pos <- data.frame(ann_final$POS)
 colnames(ann_pos) <- c("position")
 protein_pos = list()
 for (i in 1:nrow(ann_pos)) {
-  if (ann_pos$position[i] >= 1 & ann_pos$position[i] < 743) {
+  if(ann_pos$position[i] >= 1 & ann_pos$position[i] < 743) {
     protein_pos = append(protein_pos, "5'-UTR")
   } else if (ann_pos$position[i] >= 743 & ann_pos$position[i] <= 949) {
     protein_pos = append(protein_pos,"VP4")
@@ -121,15 +122,14 @@ for (i in 1:nrow(ann_pos)) {
   } else if (ann_pos$position[i] >= 5987 & ann_pos$position[i] <= 7369) {
     protein_pos = append(protein_pos,"3D")
   } else {
-    print("undefined region")
+    protein_pos = append(protein_pos,"undefined region")
   }
 }
-#print(unlist(protein_pos))
-ann_final <- data.frame(paste0(ann_final$POS, ann_final$ALT, if(display_as){paste0(" ", "(", ann_final$EFF....AA, ")")}), ann_final$EFF....EFFECT, unlist(protein_pos))
-colnames(ann_final) <- c("position","effect", "gene")
+ann_final <- data.frame(paste0(ann_final$POS, ann_final$ALT),unlist(protein_pos))
+colnames(ann_final) <- c("position", "gene")
 
 for(i in 2:length(files)) {
-  ann <- read.table(files[i], header = T, sep = "\t")
+  ann <- read.table(files[i], header = T, sep = "\t", fill = T)
   ann_pos <- data.frame(ann$POS)
   colnames(ann_pos) <- c("position")
   protein_pos = list()
@@ -162,9 +162,8 @@ for(i in 2:length(files)) {
       protein_pos = append(protein_pos,"undefined region")
     }
   }
-  
-  ann <- data.frame(paste0(ann$POS, ann$ALT, if(display_as){paste0(" ", "(", ann$EFF....AA, ")")}), ann$EFF....EFFECT, unlist(protein_pos))
-  colnames(ann) <- c("position","effect", "gene")
+  ann <- data.frame(paste0(ann$POS, ann$ALT), unlist(protein_pos))
+  colnames(ann) <- c("position", "gene")
   ann_final <- rbind(ann_final, ann)
 }
 
@@ -174,37 +173,10 @@ ann_final<-data.frame(unique(ann_final))
 ann_final <- ann_final[ann_final$position %in% colnames(final),]
 ## sort
 ann_final <- ann_final[str_order(ann_final$position, numeric = T),]
-
-## set rownames
-row.names(ann_final) <- ann_final$position
-ann_final <- ann_final[,-1]
-
-## rename annotations
-ann_final$effect <- sub("^$", "non-coding", ann_final$effect)
-ann_final$gene <- sub("^$", "NCR", ann_final$gene)
-
-ann_final$effect[ann_final$effect=="NON_SYNONYMOUS_CODING"] <- "non-syn"
-ann_final$effect[ann_final$effect=="SYNONYMOUS_CODING"] <- "syn"
-ann_final$effect[ann_final$effect=="CODON_CHANGE_PLUS_CODON_DELETION"] <- "deletion"
-ann_final$effect[ann_final$effect=="CODON_DELETION"] <- "deletion"
-ann_final$effect[ann_final$effect=="FRAME_SHIFT"] <- "frame shift"
-ann_final$effect[ann_final$effect=="STOP_GAINED"] <- "stop gained"
-ann_final$effect[ann_final$effect=="CODON_DELETION"] <- "deletion"
-ann_final$effect[ann_final$effect=="FRAME_SHIFT+STOP_GAINED"] <- "stop gained"
-ann_final$effect[ann_final$effect=="CODON_CHANGE_PLUS_CODON_INSERTION"] <- "insertion"
-ann_final$effect[ann_final$effect=="FRAME_SHIFT+STOP_LOST+SPLICE_SITE_REGION"] <- "frame shift"
-ann_final$effect[ann_final$effect=="INSERTION"] <- "insertion"
-ann_final$effect[ann_final$effect=="START_LOST"] <- "non-syn"
-ann_final$effect[ann_final$effect=="STOP_LOST+SPLICE_SITE_REGION"] <- "non-syn"
-ann_final$effect[ann_final$effect=="GENE_FUSION"] <- "non-syn"
-ann_final$effect[ann_final$effect=="upstream_gene_variant"] <- "upstream gene variant"
-ann_final$effect[ann_final$effect=="synonymous_variant"] <- "syn-variant"
-ann_final$effect[ann_final$effect=="stop_gained"] <- "stop gained"
-ann_final$effect[ann_final$effect=="missense_variant"] <- "missense variant"
-ann_final$effect[ann_final$effect=="frameshift_variant+missense_variant"] <- "frame shift"
+## reshape for annotation_col in pheatmap
+ann_final <- data.frame(ann_final[,-1, drop = FALSE], row.names = ann_final$position)
 
 # automatically determine gaps for the heatmap
-
 gap_vector <- c()
 
 for (i in 2:length(ann_final$gene)){
@@ -223,68 +195,24 @@ count <- length(unique(ann_final$gene))
 gene_color <- c(brewer.pal(color_gene_annotation, n=count))
 names(gene_color) = unique(ann_final$gene)
 
-## colormanagement annotations (effect)
-
-colors <- c()
-
-if(c("non-coding") %in% ann_final$effect){
-  colors <- c(colors, "white")
-}
-if(c("syn") %in% ann_final$effect){
-  colors <- c(colors, "green")
-}
-if(c("non-syn") %in% ann_final$effect){
-  colors <- c(colors, "orange")
-}
-if(c("deletion") %in% ann_final$effect){
-  colors <- c(colors, "red")
-}
-if(c("frame shift") %in% ann_final$effect){
-  colors <- c(colors, "black")
-}
-if(c("stop gained") %in% ann_final$effect){
-  colors <- c(colors, "grey")
-}
-if(c("insertion") %in% ann_final$effect){
-  colors <- c(colors, "blue")
-}
-if(c("syn-variant") %in% ann_final$effect){
-  colors <- c(colors, "cyan")
-}
-if(c("missense variant") %in% ann_final$effect){
-  colors <- c(colors, "magenta")
-}
-if(c("upstream gene variant") %in% ann_final$effect){
-  colors <- c(colors, "yellow")
-}
-
-all_colors<-data.frame(color=c("white", "green", "orange", "red", "black", "grey", "blue", "cyan","magenta","yellow" ), 
-                       name = c("non-coding", "syn", "non-syn", "deletion", "frame shift", "stop gained", "insertion", "syn-variant", "missense variant", "upstream gene variant"))
-
-subset_colors<-subset(all_colors, color %in% colors)
-
-effect_color <- subset_colors$color
-names(effect_color) = subset_colors$name
-
-color_list <- list(gene_color = gene_color, effect_color = effect_color)
-names(color_list) <- c("gene", "effect")
+color_list <- list(gene_color = gene_color)
+names(color_list) <- c("gene")
 
 
 # visualize heatmap
 
 ## The pdf scales with the number of variants and samples
-pdf(paste0(name_plot, "(", frequency, ")", ".pdf"), width = multiplier_width*ncol(final), height = multiplier_height*nrow(final))
+# pdf(paste0(name_plot, "(", frequency, ")", ".pdf"), width = multiplier_width*ncol(final), height = multiplier_height*nrow(final))
 
 pheatmap(final, 
          color = my_colors(100),
-         cellwidth = 20,
-         cellheight = 20,
+         #cellwidth = 20,
+         #cellheight = 20,
          clustering_method = clustering_method,
          cluster_rows = clustering, 
          cluster_cols = F,
          cutree_rows = number_of_clusters,
          annotation_col = ann_final,
-         annotation_colors = color_list,
          gaps_col = gap_vector,
          display_numbers = final_rounded
          )
@@ -294,7 +222,7 @@ dev.off()
 ## file checker - checks if files have the expected column count of 12
 for (i in 1:length(files)) {
   x <- fread(files[i], header = T, sep = "\t", fill = T)
-  if (ncol(x) == 12){
+  if (ncol(x) == 6){
     print(paste(files[i], "...............", "ok"))
   } else {
     print(paste(files[i], "...............", "error")) 
